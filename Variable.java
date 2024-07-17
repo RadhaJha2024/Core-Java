5630ea0fb85927f42d807ae082b7708ebadbc590
RadhaJha2024 <radhajha1702@gmail.com>
2024-07-17 19:03:12 +0530

    First Day

---
+++ Variable.java
@@ -0,0 +1,46 @@
+//building blocks of java; data are stored in variables
+
+public class Variable {
+    public static void main(String[] args)
+    { 
+        int num1 = 3;
+        int num2= 5;
+        int num3=6;
+        System.out.println(num1+num2-num3);
+
+        int num4= 4;
+        int num5= 5;
+        int result= num4+num5;
+        System.out.println(result);
+
+
+        System.out.println(9+133);
+        System.out.println(899999+4823983);
+    
+    
+//datatypes- integer, float, character, boolean
+double value= 7.88888;
+float value2= 9.99f;
+System.out.println(value-value2);
+
+
+int num=10;
+byte by= 127;
+System.out.println(num+by);
+int a =22;
+int b=10;
+int sum= a +b;
+System.out.println(sum);
+String name="radhe";
+char x='s';
+int diff= b-a;
+int mul=a*b;
+System.out.println(name);
+System.out.println(x);
+System.out.println(a);
+System.out.println(diff);
+System.out.println(mul);
+System.out.println(a*b/a-b);
+// System.out.println((a*b)/(a-b));
+    }
+}
