701cd0bf7fbabd8969aa7aeac85e7f1ca719813f
RadhaJha2024 <radhajha1702@gmail.com>
2024-07-18 23:20:31 +0530

    "Literals"

---
+++ Literal.java
@@ -0,0 +1,20 @@
+public class Literal {
+    public static void main(String[] args){
+    int num1= 0b101;//101 is binary of 5
+    int num2= 0x7E;// x for hexadecimals
+    int num3=10_0000;// underscore helps counting zeros during codinng only
+    double num4=56e10;
+    char c='a';
+    c++;
+    c--;
+
+    System.out.println(num1);
+    System.out.println(num2);
+    System.out.println(num3);
+    System.out.println(num4);
+    System.out.println(c);
+    
+    
+
+}
+}
