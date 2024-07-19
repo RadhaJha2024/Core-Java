d966d5187cee8b6515397e3d186c5e82ffb4264b
RadhaJha2024 <radhajha1702@gmail.com>
2024-07-19 22:21:39 +0530

    Learning the basics

---
+++ Output.java
@@ -0,0 +1,12 @@
+public class Output {
+    public static void main(String [] args){
+        System.out.println("hello");
+        System.out.print("hiii\n");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println(("****"));
+
+    }
+    
+}
