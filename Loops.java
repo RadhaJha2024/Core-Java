9449d0564c206db1aae824ac97ac12c76f89c6ec
RadhaJha2024 <radhajha1702@gmail.com>
2024-07-20 10:06:57 +0530

    Loops

---
+++ Loops.java
@@ -0,0 +1,87 @@
+/*
+
+public class Loops {
+    // while loop, do while loop, for loop;
+
+    public static void main(String[]args){
+        // while loop
+        int i =1;
+        while(i<=10)
+        {
+            System.out.println("hellow"+ i);
+            int j=1;
+            while(j<4){
+            System.out.println("hellowwwaa");
+            j++;
+        } 
+        i++;
+    }
+        System.out.println("byee");}
+    
+}
+
+*/
+/* 
+public class Loops{
+    public static void main(String[] args)
+{
+    int i=5;
+    do{
+        System.out.println("helloww"+i);
+        i++;
+
+    }
+
+
+while(i<=4);
+
+}
+}
+*/
+
+
+//for loop
+/*public class Loops{
+    public static void main(String[] args){
+     int i=0;
+        for(i=0;i<=5;i++){
+            System.out.println("hii"+i);
+            int j=1;
+              for (j=1;j<=2;j++){
+                System.out.println("byee"+j);
+                j++;
+              }
+        }
+        i++;
+    }
+}
+*/
+
+/*public class Loops{
+    public static void main(String[] args){
+        for(int i=0;i<=4;i++){
+            System.out.println("hi radha" +i);
+
+        for(int i=4; i>=1;i--){
+            System.out.println("bye"+i);
+        }
+    }
+}*/
+
+public class Loops{
+    public static void main(String[] args){
+// printing number of days with 24 hours in each day
+for(int i=1;i<8;i++){
+    System.out.println("day" +i);
+     for(int j=1;j<24;j++){
+        System.out.println("    hour"  +(j) +"-" + (j+1));
+     }
+}
+
+    }
+}
+
+
+//if u know the number of iterations, use for loop
+//reading a file, use while loop; do while based on a condition
+
