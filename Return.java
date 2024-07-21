beaf1629a6afcd4b6b096df2448556c34f59020b
RadhaJha2024 <radhajha1702@gmail.com>
2024-07-21 13:36:50 +0530

    return

---
+++ Return.java
@@ -0,0 +1,13 @@
+public class Return {
+    static int method(int x,int y){
+    return x-y;
+  
+
+    }
+    
+
+    public static void main(String[] args){
+       System.out.println(method(5,3));
+     // or,we can provide (method(5,3))to a variable,say z and then we can print z in syso which is inbuilt method
+    }
+}
