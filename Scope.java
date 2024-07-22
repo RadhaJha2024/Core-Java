66834ea1e13a792e8a597febf200293078f7fa79
RadhaJha2024 <radhajha1702@gmail.com>
2024-07-22 22:21:14 +0530

    scope

---
+++ Scope.java
@@ -0,0 +1,10 @@
+public class Scope {
+    //variables are acceessible only inside the region they have been created;this is called as scope
+
+        public static void main(String[] args){
+            int x=1000;
+
+            System.out.println(x);
+        }
+    }
+
