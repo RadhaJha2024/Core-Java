30c5807b2be8e4d982f5ec818e3aff9a57fd705f
RadhaJha2024 <radhajha1702@gmail.com>
2024-07-22 22:25:19 +0530

    patterns2

---
+++ Patterns2.java
@@ -0,0 +1,33 @@
+public class Patterns2 {
+    
+/*
+
+public class Patterns{
+    public static void main(String[] args){
+        for(int i=0;i<6;i++){
+            for(int j=6;j>i;j--){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
+*/
+
+
+/*
+
+public class Patterns{
+    public static void main(String[] args){
+        for(int i=0;i<6;i++){
+            for(int j=6;j>i;j--){
+                System.out.print(6-j+1);
+            }
+            System.out.println();
+        }
+    }
+}
+
+*/
+
+}
