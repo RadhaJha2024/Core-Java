3f5e148d569e7202317a4ca992ca3b454e0be4f2
RadhaJha2024 <radhajha1702@gmail.com>
2024-07-22 22:24:48 +0530

    patterns

---
+++ Patterns.java
@@ -0,0 +1,88 @@
+/*
+class Patterns {
+    // 4*4 pattern
+   
+   public static void main(String[] args)
+   {
+        for(int i=0; i<4; i++)
+        {
+           
+            for(int j=0; j<4; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        
+        }
+        
+}
+}*/
+
+// half pyramid
+
+/*public class Patterns{
+    public static void main(String[] args){
+        for(int i=0;i<5;i++){
+            for(int j=0;j<i;j++){
+                System.out.print("*" );
+            }
+            System.out.println();
+        }
+    }
+}*/
+
+//half pyramid with numbers
+/*public class Patterns{
+    public static void main(String[] args){
+        for(int i=0;i<5;i++){
+            for(int j=0;j<i;j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}*/
+
+/*
+public class Patterns{
+    public static void main(String[] args){
+        for(int i=0;i<5;i++){
+            for(int j=1;j<i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
+*/
+
+//downward hill
+
+/*
+public class Patterns{
+    public static void main(String[] args){
+        for(int i=0;i<5;i++){
+            for(int j=5;j>i;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+*/
+
+//downward hill with numbers
+/*
+
+public class Patterns{
+    public static void main(String[] args){
+        for(int i=0;i<6;i++){
+            for(int j=5;j>i;j--){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
+
+*/
