7f878b9e1dd59eba09bc9ebb580272796bff9785
RadhaJha2024 <radhajha1702@gmail.com>
2024-08-24 12:43:13 +0530

    Enum.java

---
+++ Enum.java
@@ -0,0 +1,18 @@
+enum Status
+{
+    running, notWorking, success; //named constants
+}
+
+
+public class Enum {
+    public static void main(String[] args)
+{
+    int i=5;
+    
+    Status s= Status.running;
+   
+    
+    System.out.println(s);
+}
+
+}
