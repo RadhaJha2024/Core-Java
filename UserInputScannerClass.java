59020d11177a93810a55fd47210f946a324874b9
RadhaJha2024 <radhajha1702@gmail.com>
2024-09-08 20:50:20 +0530

    UserInputScannerClass

---
+++ UserInputScannerClass.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+
+public class UserInputScannerClass {
+ public static void main(String[] args) {
+    System.out.println();
+Scanner sc= new Scanner(System.in);
+int num= sc.nextInt();
+System.out.println(num);
+
+ }   
+}
