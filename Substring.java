4309b236948be7a9e7b80d359b1ec947fdd32d48
RadhaJha2024 <radhajha1702@gmail.com>
2024-09-19 14:06:55 +0530

    Substring

---
+++ Substring.java
@@ -0,0 +1,8 @@
+public class Substring{    
+    public static void main(String args[]){    
+    String s="RadhaJha";    
+    System.out.println("Original String: " + s);  
+    System.out.println("Substring starting from index 6: " +s.substring(6));   
+    System.out.println("Substring starting from index 0 to 6: "+s.substring(0,6));   
+    }  
+   }    
